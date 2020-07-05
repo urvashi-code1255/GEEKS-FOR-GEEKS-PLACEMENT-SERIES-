5962be68dd9cf7ec63dce607288ca77ea0d3d989
Urvashi Anand <63122133+urvashi-code1255@users.noreply.github.com>
2020-07-05 21:50:03 +0530

    Merge_Sort.cpp

--- Searching_and_Sorting/Merge_Sort.cpp
+++ Searching_and_Sorting/Merge_Sort.cpp
@@ -6,6 +6,7 @@ void merge(int arr[], int l, int m, int r)
     
     for(int i = 0 ; i < n1 ; i++ )
         L[i] = arr[l + i];
+    
     for(int j = 0 ; j < n2 ; j++)
         R[j] = arr[m + 1 + j] ;
     
