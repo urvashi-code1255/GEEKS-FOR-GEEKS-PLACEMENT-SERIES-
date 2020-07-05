a96091696aa8fe6c756b28e5fc4e975a19abf3de
urvashi-code1255 <urvashianand0906@gmail.com>
2020-07-05 17:37:13 +0530

    p

---
+++ Searching_and_Sorting/Square_Root.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long int floor(long long int a)
+{
+    int start = 0 , end = int(a) , mid , res = 0 ;
+    while(start <= end)
+    {
+        mid = start + (end - start)/2 ;
+        if(mid == a)
+            return mid ;
+        else if(mid < a)
+        {
+            res = mid ;
+            start = mid + 1 ;
+        }
+        else
+            end = mid - 1 ;
+    }
+    return res ;
+}
+
+long long int PerfectSquare(long long int x)
+{
+    int sq = sqrt(x);
+    return (sq - floor(x) == 0);
+}
+
+long long int floorSqrt(long long int x) 
+{
+    if(PerfectSquare)
+        return sqrt(x);
+    else
+        return floor(sqrt(x));
+}
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long n;
+		cin>>n;
+		cout << floorSqrt(n) << endl;
+	}
+    return 0;   
+}
